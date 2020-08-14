a9c472a22297e1ea5f09a5efacb80b3ae3fa4b07
Enrico Sorcinelli <enrico.sorcinelli@italiaonline.it>
2020-08-14 17:58:22 +0200

    PHPCS fixes.

--- php/adminpages/settings.php
+++ php/adminpages/settings.php
@@ -302,8 +302,8 @@ function cb_list( $args = array() ) {
 					<textarea name="<?php echo esc_attr( $params['prefix'] . 'settings[xmlrpc][trusted_networks]' ); ?>" rows="5" cols="50" id="<?php echo esc_attr( $params['prefix'] . 'settings_xmlrpc_trusted_networks' ); ?>" class="large-text code"><?php echo esc_html( $params['settings']['xmlrpc']['trusted_networks'] ); ?></textarea>
 					<p class="description">
 						<?php
-							/* translators: 1: IP/Networks example, 2: comment syntax, 3: comment syntax */ 
-							echo sprintf( esc_html__( 'For example: %1$s . You can add also PHP single line comments (using %2$s or %3$s).', 'rest-xmlrpc-data-checker' ), '<code>10.10.0.1/32, 10.10.10.0/25</code>', '<code>//</code>', '<code>#</code>' ); 
+							/* translators: 1: IP/Networks example, 2: comment syntax, 3: comment syntax */
+							echo sprintf( esc_html__( 'For example: %1$s . You can add also PHP single line comments (using %2$s or %3$s).', 'rest-xmlrpc-data-checker' ), '<code>10.10.0.1/32, 10.10.10.0/25</code>', '<code>//</code>', '<code>#</code>' );
 						?>
 					</p>
 				</td>
