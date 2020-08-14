9dba5e6f5453faf6d7d3c4982117a84514e73ebb
Enrico Sorcinelli <enrico.sorcinelli@italiaonline.it>
2020-08-14 17:56:11 +0200

    Coding standard fixes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,31 +1,33 @@
 /**
  * @file This file contains XML-RPC REST Data Checker JavaScript
  * @author Enrico Sorcinelli <enrico.sorcinelli.com>
- * @version 0.0.1
+ * @version 1.3.2
  * @title XML-RPC REST Data Checker
  */
 
 // Make all in a closure.
-;(function ($) {
+;( function( $ ) {
 	$( document ).ready( function() {
 
 		// Tab managements.
-		$( '.nav-tab-wrapper.rest-xmlrpc-data-checker a' ).on( 'click', function () {
+		$( '.nav-tab-wrapper.rest-xmlrpc-data-checker a' ).on( 'click', function() {
 			$( '.nav-tab.rest-xmlrpc-data-checker' ).removeClass( 'nav-tab-active' );
 			$( this ).addClass( 'nav-tab-active' );
 			$( 'section.rest-xmlrpc-data-checker' )
 				.hide()
 				.eq( $( this ).index() ).show();
 			$( 'input[name="' + rest_xmlrpc_data_checker_i18n.prefix + 'active_tab"]' ).val( $( this ).data( 'section' ) );
+
 			document.location.hash = $( this ).data( 'section' );
 			return false;
 		});
 
 		// Active tab by hash.
-		$( '.nav-tab-wrapper.rest-xmlrpc-data-checker a[data-section="' + document.location.hash.replace( /^#+/, '' ) + '"]' ).trigger('click');
+		$( '.nav-tab-wrapper.rest-xmlrpc-data-checker a[data-section="' + document.location.hash.replace( /^#+/, '' ) + '"]' ).trigger( 'click' );
 
 		// Add handler on click over user row.
-		$( 'table.rest-xmlrpc-data-checker-users-list.cb tbody tr td' ).on( 'click', function(e) {
+		$( 'table.rest-xmlrpc-data-checker-users-list.cb tbody tr td' ).on( 'click', function( e ) {
+
 			//$( e.currentTarget ).parent().toggleClass( 'selected' );
 			$( e.currentTarget ).parent().find( 'th input' ).first().trigger( 'click' );
 		} );
@@ -35,7 +37,7 @@
 
 			// REST API warning.
 			if ( 'basic_auth' === $( 'input[name="' + rest_xmlrpc_data_checker_i18n.prefix + 'settings\\[rest\\]\\[auth_method\\]"]:checked' ).val() ) {
-				$( 'div.update-nag.rest_basic_auth_alert' ).show()
+				$( 'div.update-nag.rest_basic_auth_alert' ).show();
 			}
 			else {
 				$( '.' + rest_xmlrpc_data_checker_i18n.prefix + 'rest_enable' ).hide();
@@ -62,7 +64,7 @@
 		}
 
 		// Handle toggle over methods/end-point.
-		$( '.rest-xmlrpc-data-checker-cb-list h2 span' ).on( 'click', function( e ) {
+		$( '.rest-xmlrpc-data-checker-cb-list h2 span' ).on( 'click', function() {
 			$( this )
 				.toggleClass( 'closed' )
 				.parent().next().toggle( 200 );
@@ -70,7 +72,7 @@
 		} );
 
 		// Check all/none over methods/end-point.
-		$( '.rest-xmlrpc-data-checker-cb-list h2' ).find( 'input[type="checkbox"]:first' ).on( 'click', function( e ) {
+		$( '.rest-xmlrpc-data-checker-cb-list h2' ).find( 'input[type="checkbox"]:first' ).on( 'click', function() {
 			$( this ).removeClass( 'indeterminate' );
 			$( this ).parent().next().find( 'input[type="checkbox"]' ).prop( 'checked', $( this ).prop( 'checked' ) );
 		} );
@@ -119,7 +121,7 @@
 		// Check parent checkbox on load.
 		$( '.rest-xmlrpc-data-checker-cb-list ul' ).each( function() {
 			( $( 'input[type="checkbox"]:first', this ) ).each( check_all_checkbox );
-		} )
+		} );
 
 	});
 })( jQuery );
